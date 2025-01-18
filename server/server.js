b275e374948f98e4af048dd59b58a19ec7c06557
pankajkeshari099 <85383471+pankajkeshari099@users.noreply.github.com>
2025-01-18 23:29:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,16 +5,19 @@ const connectDB = require('./config/db-connection');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/api', require('./routes/routes'));
-
-connectDB();
+app.use('/api', require('./routes/index.js'));
 
 const PORT = process.env.SERVER_PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+    try {
+        app.listen(PORT, () => {
+            console.log("Server running on port ", process.env.SERVER_PORT);
+            console.log("connection successful");
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
