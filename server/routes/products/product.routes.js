a3c184be1c2170690aab03d7391ef80aaf9ede29
pankajkeshari099 <85383471+pankajkeshari099@users.noreply.github.com>
2025-01-18 23:25:32 +0530

    Delete server/routes directory

--- server/routes/products/product.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router({caseSensitive : true});
-const productController = require('../../controller/product.js')
-
-routes.post('/addProduct', productController.addProduct);
-routes.get('/getProducts', productController.getProducts);
-routes.get('/getProduct/:id', productController.getProduct);
-module.exports = routes;
--- server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const routes = express.Router({caseSensitive : true});
-
-// routes.use('/authentication/',require('../routes/authentication/authentication.routes.js'))
-routes.use('/user/',require('../routes/users/user.routes.js'))
-routes.use('/product/',require('../routes/products/product.routes.js'))
-module.exports = routes;
