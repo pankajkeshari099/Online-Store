621a71b204aa9bf1853bde62af8423815dc9b818
pankajkeshari099 <85383471+pankajkeshari099@users.noreply.github.com>
2025-01-18 23:20:18 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,69 @@
 import './App.css';
-import AuthVerify from './common/AuthVerify';
-import Cart from './components/Cart';
-import Home from './components/Home';
-import Login from './components/Login';
-import Registration from './components/Registration';
-import {BrowserRouter, Routes, Route} from "react-router";
-import { ToastContainer} from 'react-toastify';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import About from './components/About';
+import Signup from './components/Signup';
+import AuthVerify from './helpers/AuthVerify';
 import Profile from './components/Profile';
+import AddProduct from './components/addProduct';
+import UserList from './components/UserList';
+import ViewProduct from './components/ViewProduct';
+import Product from './components/Products';
+import CartList from './components/Cart';
+import axios from 'axios';
+import { useEffect } from 'react';
+import Context from './context';
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from './store/userSlice';
 import Order from './components/Order';
-import Help from './components/Help';
+
 function App() {
+  const dispatch = useDispatch();
+  const loadUserProfile = async () => {
+    try {
+      let token = localStorage.getItem('token');
+      const response = await axios.get(`/api/user/getUserProfile`,{
+        headers:{
+          Authorization: `Bearer ${token}`
+        }
+      });
+      const result =  response.data.user;
+      dispatch(setUserDetails(result));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      loadUserProfile();
+    }
+  }, []);
   return (
-    <div>
-      <ToastContainer />
-      <BrowserRouter>
-      <AuthVerify/>
-        <Routes>
-            <Route path="/" element={<Login/>}/>
-            <Route path="/registration" element={<Registration/>}/>
-            <Route path='/home' element={<Home/>}></Route>
-            <Route path='/cart' element={<Cart/>}></Route>
-            <Route path='/profile' element={<Profile/>}></Route>
-            <Route path='/order' element={<Order/>}></Route>
-            <Route path='/help' element={<Help/>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <Context.Provider value={{loadUserProfile}}>
+        <ToastContainer />
+        <BrowserRouter>
+          <AuthVerify />
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path='/product/:type' element={<Product />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/addProduct' element={<AddProduct />} />
+            <Route path='/userList' element={<UserList />} />
+            <Route path='/viewProduct/:id' element={<ViewProduct />} />
+            <Route path='/cart' element={<CartList />} />
+            <Route path='/order' element={<Order />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
+    </>
   );
 }
 
